0c6f091276bf4bfc0893db28989a3119603d18fe
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-01-07 14:56:22 +0100

    Initial setup

---
+++ src/main/java/com/bgasparotto/filedownloader/FileDownloaderApplication.java
@@ -0,0 +1,12 @@
+package com.bgasparotto.filedownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDownloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileDownloaderApplication.class, args);
+    }
+}
