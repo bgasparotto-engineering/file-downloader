88b1b9e0ceef6f7139a9be0578f5649c44d9ee85
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-01-17 14:52:39 +0100

    Parameterised the output folder and improved the resource management.

--- src/main/java/com/bgasparotto/filedownloader/service/FileStreamerService.java
+++ src/main/java/com/bgasparotto/filedownloader/service/FileStreamerService.java
@@ -4,15 +4,14 @@ import com.bgasparotto.filedownloader.message.DownloadableFile;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
-import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.InputStream;
 import java.net.URI;
 
 import static org.apache.commons.lang3.StringUtils.substringAfterLast;
@@ -24,19 +23,21 @@ public class FileStreamerService {
     private final RestTemplate restTemplate;
     private final FileSystem hdfs;
 
+    @Value("${hdfs.output.path}")
+    private String outputPath;
+
     public Path stream(DownloadableFile downloadableFile) {
         final Path path = pathForFile(downloadableFile);
 
         /* Streams the file from the source URL directly to HDFS. */
         ResponseExtractor<Void> responseExtractor = response -> {
-            InputStream inputStream = response.getBody();
-            FSDataOutputStream outputStream = hdfs.create(path);
+            try (var inputStream = response.getBody(); var outputStream = hdfs.create(path)) {
+                log.info("Downloading file {} into HDFS...", path);
+                IOUtils.copy(inputStream, outputStream);
 
-            IOUtils.copy(inputStream, outputStream);
-            inputStream.close();
-            outputStream.close();
-
-            return null;
+                log.info("Successfully downloaded file {} into HDFS.", path);
+                return null;
+            }
         };
         restTemplate.execute(uriForFile(downloadableFile), HttpMethod.GET, null, responseExtractor);
 
@@ -49,6 +50,6 @@ public class FileStreamerService {
 
     private Path pathForFile(DownloadableFile downloadableFile) {
         String fileName = substringAfterLast(downloadableFile.getId(), "/");
-        return new Path(fileName);
+        return new Path(outputPath.concat(fileName));
     }
 }
