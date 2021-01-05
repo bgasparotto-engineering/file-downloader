d0a80f4597218fb9d649cbc58c5ed079b97512fc
bgasparotto <bruno.m.gasparotto@gmail.com>
2021-01-05 14:26:48 +0100

    Moved the integration tests to a new source set, triggered by the Gradle task integrationTest.

--- src/it/java/com/bgasparotto/filedownloader/config/IntegrationTestConfiguration.java
+++ src/it/java/com/bgasparotto/filedownloader/config/IntegrationTestConfiguration.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("test")
+@Profile("it")
 @Configuration
 @ComponentScan("com.bgasparotto.spring.kafka.avro.test")
-public class TestConfiguration {
+public class IntegrationTestConfiguration {
 }
--- src/it/java/com/bgasparotto/filedownloader/service/DistributedFileSystemServiceTest.java
+++ src/it/java/com/bgasparotto/filedownloader/service/DistributedFileSystemServiceTest.java
@@ -22,7 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("it")
 public class DistributedFileSystemServiceTest {
 
     @Autowired
